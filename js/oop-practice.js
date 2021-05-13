7336c9216193edd414c3edab1ccb0e254779a60a
jmaine22 <jmaine22@gmail.com>
2021-05-12 17:19:02 -0700

    assignment12

---
+++ js/oop-practice.js
@@ -0,0 +1,37 @@
+// STEP 1
+class Cat {
+    constructor(){
+
+    }   
+}
+
+function Dog() {
+
+}
+// STEP 2
+const cat = new Cat();
+const dog = new Dog();
+// STEP 3
+// class Animal {
+//     constructor(){
+//         console.log ('The Animal has been created.');
+//     }
+// }
+// STEP 4
+// class Animal {
+//     constructor(type){
+//         this.type = type;
+//         console.log ('The ' + type + ' has been created.');
+//     }
+// }
+// const mouse = new Animal('mouse');
+// const lion = new Animal('lion');
+// STEP 5
+
+// STEP 6
+
+// STEP 7
+
+// STEP 8
+
+// STEP 9
