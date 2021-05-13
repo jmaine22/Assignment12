9bd74d6e578777d4b29ad1476d39ef2e831652db
jmaine22 <jmaine22@gmail.com>
2021-05-12 18:20:40 -0700

    assignment12

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -27,9 +27,28 @@ const dog = new Dog();
 // const mouse = new Animal('mouse');
 // const lion = new Animal('lion');
 // STEP 5
-
+// class Animal {
+//     constructor(type, breed, color, height, length){
+//         this.type = type;
+//         this.breed = breed;
+//         this.color = color;
+//         this.height = height;
+//         this.length = length;
+//         console.log("The animal is a " + type + " the breed is " + breed + " , the color is " + color + " and it is " + height + " tall and " + length + " in length.")
+//     }
+// }
+// const cheetah = new Animal('cheetah', 'Tanzanian', 'white-yellowish', '3.1 ft', '135 cm');
 // STEP 6
-
+class Animal {
+    constructor(type, breed, color, height, length){
+        this.type = type;
+        this.breed = breed;
+        this.color = color;
+        this.height = height;
+        this.length = length;
+    }
+    
+}
 // STEP 7
 
 // STEP 8
