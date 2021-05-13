7c7194a6c47b5efaeaada2fc33508106ebe3242e
jmaine22 <jmaine22@gmail.com>
2021-05-12 22:42:49 -0700

    assignment12

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,16 +1,16 @@
 // STEP 1
-class Cat {
-    constructor(){
+// class Cat {
+//     constructor(){
 
-    }   
-}
+//     }   
+// }
 
-function Dog() {
+// function Dog() {
 
-}
+// }
 // STEP 2
-const cat = new Cat();
-const dog = new Dog();
+// const cat = new Cat();
+// const dog = new Dog();
 // STEP 3
 // class Animal {
 //     constructor(){
@@ -39,24 +39,49 @@ const dog = new Dog();
 // }
 // const cheetah = new Animal('cheetah', 'Tanzanian', 'white-yellowish', '3.1 ft', '135 cm');
 // STEP 6
+// class Animal {
+//     constructor(type, breed, color, height, length){
+//          this.type = type;
+//          this.breed = breed;
+//          this.color = color;
+//          this.height = height;
+//          this.length = length;
+//          let animals = [this.type, this.breed, this.color, this.height, this.length]
+        
+//         for(let animal in animals){
+//             console.log(animals);
+//         }
+//     }
+// }
+// const monkey = new Animal('monkey', 'africa', 'brown', '2ft', '2.2 ft');
+// const bear = new Animal('bear', 'grizzle', 'brown', '8ft', '9 ft');
+// STEP 7
 class Animal {
     constructor(type, breed, color, height, length){
          this.type = type;
          this.breed = breed;
          this.color = color;
          this.height = height;
-         this.length = length;
+         this.length =  length;
          let animals = [this.type, this.breed, this.color, this.height, this.length]
         
         for(let animal in animals){
-            console.log(animals);
+            console.log(animals[animal]);
+        }
+    }
+    speak = function () {
+        if (this.type == 'dog') {
+            //console.log(this.type);
+           console.log( 'The ' +  this.color + " " + this.type + ' is barking!');
+        }else if (this.type == 'cat') {
+            console.log( 'The ' + this.color + " " + this.type + ' is meowing!');
         }
     }
 }
-const monkey = new Animal('monkey', 'africa', 'brown', '2ft', '2.2 ft');
-const bear = new Animal('bear', 'grizzle', 'brown', '8ft', '9 ft');
-// STEP 7
-
+const cat = new Animal('cat', 'pet', 'black', '2ft', '2ft');
+cat.speak();
+const dog = new Animal('dog', 'pit bull', 'brown and white', '3ft', '3 ft');
+dog.speak();
 // STEP 8
 
 // STEP 9
