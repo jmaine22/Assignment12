20dca181ca941da11d32d91e1351588485ae38bc
jmaine22 <jmaine22@gmail.com>
2021-05-13 04:09:56 -0700

    assignment12

--- js/recipe-card.js
+++ js/recipe-card.js
@@ -4,6 +4,7 @@ const recipe = {
     ingredients: ['1 pound ground beef', '1 onion, chopped', '1 small green bell pepper, diced', 
     '1 can of spaghetti sauce', 'spaghetti pasta'],
     output: function(){
-        console.log( this.title + " " + this.serving + " " + this.ingredients);
+        console.log( this.title + "\n " + 'Serves: ' + this.serving + "\n " + 'Ingredients: ' + this.ingredients);
     }
 }
+recipe.output();
